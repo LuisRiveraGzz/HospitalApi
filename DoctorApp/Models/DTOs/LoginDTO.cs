d46f096f3babecc38a8a9b32fd786a569470fa2b
HectorContrerasD <201g0239@rcarbonifera.tecnm.mx>
2024-06-17 22:51:28 -0600

    avance doctor

---
+++ DoctorApp/Models/DTOs/LoginDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoctorApp.Models.DTOs
+{
+    public class LoginDTO
+    {
+        public string Usuario { get; set; } = null!;
+        public string Contraseña { get; set; } = null!;
+    }
+}
---
+++ DoctorApp/Models/Validators/LoginValidator.cs
@@ -0,0 +1,24 @@
+﻿using DoctorApp.Models.DTOs;
+using FluentValidation;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoctorApp.Models.Validators
+{
+    public static class LoginValidator
+    {
+        public static ValidationResult Validate (LoginDTO login)
+        {
+            var validator = new InlineValidator<LoginDTO>();
+            validator.RuleFor(x => x.Contraseña).NotEmpty().WithMessage("La contraseña no puede estar vacía");
+            validator.RuleFor(x => x.Usuario).NotEmpty().WithMessage("El nombre de usuario no puede estar vacío");
+
+            return validator.Validate(login);
+
+        }
+    }
+}
