d514cb7619a074b7b59f21fe24eff5344b8039f9
Luis Roberto Rivera Gonzalez <181G0258@rcarbonifera.tecnm.mx>
2024-06-22 16:41:13 -0600

    ya deberia estar el funcionando la app de los paceintes

---
+++ PacienteApp/Services/ApiService.cs
@@ -0,0 +1,25 @@
+﻿using PacienteApp.Models.DTOs;
+using System.Net.Http.Json;
+
+namespace PacienteApp.Services
+{
+    public class ApiService
+    {
+        private readonly HttpClient Client = new()
+        {
+            BaseAddress = new Uri("https://hospitalapi.websitos256.com/api/Pacientes/")
+        };
+        public async Task AgregarPaciente(PacienteDTO dto)
+        {
+            try
+            {
+                var response = await Client.PostAsJsonAsync("Agregar", dto);
+                response.EnsureSuccessStatusCode();
+            }
+            catch (Exception ex)
+            {
+                await Shell.Current.DisplayAlert("Error", ex.Message, "Ok");
+            }
+        }
+    }
+}
--- PacienteApp/ViewModels/PacienteViewModel.cs
+++ PacienteApp/ViewModels/PacienteViewModel.cs
@@ -1,25 +1,57 @@
 ﻿
+using CommunityToolkit.Mvvm.Input;
 using Microsoft.AspNetCore.SignalR.Client;
+using PacienteApp.Models.DTOs;
+using PacienteApp.Services;
+using System.ComponentModel;
 
 namespace PacienteApp.ViewModels
 {
-    public class PacienteViewModel
+    public partial class PacienteViewModel : INotifyPropertyChanged
     {
         private HubConnection _hubConnection;
+        private readonly ApiService service = new();
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void OnPropertyChanged(string PropertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+        public PacienteDTO Paciente { get; set; } = new();
+        public string Error { get; set; } = "";
         public PacienteViewModel()
         {
+            //Conexion al Hub de SignalR
             _hubConnection = new HubConnectionBuilder()
-                .WithUrl("https://hospitalapi.websitos256.com/notificationHub")
+                .WithUrl("https://hospitalapi.websitos256.com/NotificacionHub")
                 .Build();
 
-            //Aqui se recibe el mensaje
+            //Aqui se recibe el mensaje del hub cuando el usuario sea llamado,
+            //este se identifica por un id de conexion y el hub se encarga de manejar eso
             _hubConnection.On<string>("RecibirNotificacion", (message) =>
             {
                 //Mostrar el mensaje
                 Shell.Current.DisplayAlert("Notificacion", message, "Ok");
-                //Shell.Current.GoToAsync("Registro");
-            });
 
+                Paciente.Id = 0;
+                Paciente.Nombre = "";
+                //Cambiar vista
+                Shell.Current.GoToAsync("Registro");
+            });
+        }
+        [RelayCommand]
+        public async Task RegistrarUsuario()
+        {
+            if (string.IsNullOrWhiteSpace(Paciente.Nombre))
+            {
+                Error = "Ingrese su nombre";
+            }
+            else
+            {
+                await service.AgregarPaciente(Paciente);
+                await Shell.Current.GoToAsync("MainView");
+                OnPropertyChanged(nameof(Paciente));
+            }
         }
     }
 }
