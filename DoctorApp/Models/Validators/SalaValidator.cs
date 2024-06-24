8bacf0c5dd53febe6b0da9286926e611f82030ed
Luis Roberto Rivera Gonzalez <181G0258@rcarbonifera.tecnm.mx>
2024-06-23 23:06:22 -0600

    validador

--- DoctorApp/Models/Validators/SalaValidator.cs
+++ DoctorApp/Models/Validators/SalaValidator.cs
@@ -1,22 +1,14 @@
 ﻿using DoctorApp.Models.DTOs;
 using FluentValidation;
-using FluentValidation.Results;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DoctorApp.Models.Validators
 {
-    public  class SalaValidator
+    public class SalaValidator : AbstractValidator<SalaDTO>
     {
-        public  ValidationResult Validate(SalaDTO dTO)
+        public SalaValidator()
         {
-            var validator = new InlineValidator<SalaDTO>();
-            validator.RuleFor(x => x.NumeroSala).NotEmpty().WithMessage("El número de la sala no puede estar vacío");
-            validator.RuleFor(x => x.Doctor).NotEmpty().WithMessage("El doctor no puede estar vacío");
-            return validator.Validate(dTO);
+            RuleFor(x => x.NumeroSala).NotEmpty().WithMessage("El número de la sala no puede estar vacío");
+            RuleFor(x => x.Doctor).NotEmpty().WithMessage("El doctor no puede estar vacío");
         }
     }
 }
